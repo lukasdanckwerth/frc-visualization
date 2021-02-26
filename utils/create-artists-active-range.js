f6e462ec380ff2537abc3738b3634ea0e6dda172
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-02-26 12:30:45 +0100

    add artists active range

---
+++ utils/create-artists-active-range.js
@@ -0,0 +1,25 @@
+const json = require('./file-access');
+const frcv = require('../public/js/frcv');
+const corpusJSON = require('./load-corpus').corpusJSON;
+const corpus = new frcv.Corpus(corpusJSON);
+
+let artists = corpus.artists;
+let datasets = artists.map(function (artist) {
+  let data = artist
+    .allTracks()
+    .map(function (track) {
+      return {
+        label: track.title,
+        value: 1,
+        date: track.releaseYear,
+        location: track.departmentNumber
+      }
+    })
+
+  return {
+    label: artist.name,
+    data: data
+  }
+}).filter((dataset) => dataset.data.length > 0);
+
+json.writeAsset(datasets, 'artists-active-range.json')
