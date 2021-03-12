0bcfc2c8fbeda164aca2709a78f685f0047f5849
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-03-12 14:35:27 +0100

    update workflows

--- public/js/frc.js
+++ public/js/frc.js
@@ -56,37 +56,37 @@ class Album {
 }
 
 class Artist {
-  constructor(artistJSON) {
-    this.name = artistJSON.name;
-    this.geniusId = artistJSON.geniusId;
-    this.sex = artistJSON.sex;
-    this.group = artistJSON.group;
-    this.department = artistJSON.department || artistJSON.departement;
-    this.departmentNo = artistJSON.departmentNo || artistJSON.departementNo;
-    this.departmentName = (artistJSON.department || artistJSON.departement)
+  constructor(rawJSON) {
+    this.name = rawJSON.name;
+    this.geniusId = rawJSON.geniusId;
+    this.sex = rawJSON.sex;
+    this.group = rawJSON.group;
+    this.department = rawJSON.department || rawJSON.departement;
+    this.departmentNo = rawJSON.departmentNo || rawJSON.departementNo;
+    this.departmentName = (rawJSON.department || rawJSON.departement)
       .split("(")[0]
       .trim()
       .toLowerCase();
     this.albums = [];
-    for (let i = 0; i < artistJSON.albums.length; i++) {
-      const albumJSON = artistJSON.albums[i];
+    for (let i = 0; i < rawJSON.albums.length; i++) {
+      const albumJSON = rawJSON.albums[i];
       albumJSON.departmentNo = this.departmentNo;
       albumJSON.departmentName = this.departmentName;
-      albumJSON.artistID = artistJSON.geniusId;
-      albumJSON.artist = artistJSON.name;
+      albumJSON.artistID = rawJSON.geniusId;
+      albumJSON.artist = rawJSON.name;
       const album = new Album(albumJSON);
-      album.tracks.forEach(track => track.artistID = artistJSON.geniusId);
-      album.tracks.forEach(track => track.artist = artistJSON.name);
+      album.tracks.forEach(track => track.artistID = rawJSON.geniusId);
+      album.tracks.forEach(track => track.artist = rawJSON.name);
       this.albums.push(album);
     }
     this.tracks = [];
-    for (let i = 0; i < artistJSON.tracks.length; i++) {
-      const trackJSON = artistJSON.tracks[i];
+    for (let i = 0; i < rawJSON.tracks.length; i++) {
+      const trackJSON = rawJSON.tracks[i];
       const track = new Track(trackJSON);
       track.departmentNumber = this.departmentNo;
       track.departmentName = this.departmentName;
-      track.artistID = artistJSON.geniusId;
-      track.artist = artistJSON.name;
+      track.artistID = rawJSON.geniusId;
+      track.artist = rawJSON.name;
       this.tracks.push(track);
     }
   }
