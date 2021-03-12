ea1e442b43defcf6d2a0a373a81fad7779a6285a
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-03-12 14:18:17 +0100

    update workflown

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import {Corpus} from "./model/corpus";
-import {Artist} from "./model/artist";
-import {Album} from "./model/album";
-import {Track} from "./model/track";
-
-// exports.Track = Track;
-// exports.Album = Album;
-// exports.Artist = Artist;
-// exports.Corpus = Corpus;
 
 export default Corpus;
