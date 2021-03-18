30f425773edaad3be2780cb9ade32f4dc46a17e7
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-03-18 13:13:06 +0100

    patch lotivis version

--- public/js/frc.js
+++ public/js/frc.js
@@ -1,5 +1,5 @@
 /*!
- * frc.js v1.0.22 Lukas Danckwerth
+ * frc.js v1.0.29 Lukas Danckwerth
  */
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
