473a58418c635c4db06cf81f8606de0b6f2a94f6
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-05-21 00:09:53 +0200

    add active range variations

--- src/utils/create-artists-active-range.js
+++ src/utils/create-artists-active-range.js
@@ -1,25 +1,35 @@
 const json = require('./file-access');
-const frcv = require('../../public/js/frcv');
+const frc = require('../../public/js/frc');
 const corpusJSON = require('./load-corpus').corpusJSON;
-const corpus = new frcv.Corpus(corpusJSON);
+const corpus = new frc(corpusJSON);
 
 let artists = corpus.artists;
-let datasets = artists.map(function (artist) {
-  let data = artist
-    .allTracks()
-    .map(function (track) {
-      return {
-        label: track.title,
-        value: 1,
-        date: track.releaseYear,
-        location: track.departmentNumber
-      }
-    })
+let maleArtists = corpus.maleArtists();
+let femaleArtists = corpus.femaleArtists();
+let groupArtists = corpus.groupArtists();
 
-  return {
-    label: artist.name,
-    data: data
-  }
-}).filter((dataset) => dataset.data.length > 0);
+function createDatasets(artists, name) {
+  return artists.map(function (artist) {
+    let data = artist
+      .allTracks()
+      .map(function (track) {
+        return {
+          // label: track.title,
+          value: 1,
+          date: track.releaseYear,
+          location: track.departmentNumber
+        }
+      })
 
-json.writeAsset(datasets, 'artists-active-range.json')
+    return {
+      label: artist.name,
+      data: data
+    }
+
+  }).filter((dataset) => dataset.data.length > 0);
+}
+
+json.writeAsset(createDatasets(artists), 'artists-active-range.json');
+json.writeAsset(createDatasets(maleArtists), 'artists-active-range-male.json');
+json.writeAsset(createDatasets(femaleArtists), 'artists-active-range-female.json');
+json.writeAsset(createDatasets(groupArtists), 'artists-active-range-groups.json');
