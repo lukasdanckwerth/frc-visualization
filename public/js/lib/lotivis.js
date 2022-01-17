c068cafdac1b028e13daff4b47cf5a49fcc06f73
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-01-17 17:35:57 +0100

    update lotivis

--- public/js/lib/lotivis.js
+++ public/js/lib/lotivis.js
@@ -24609,6 +24609,78 @@
     }
   }
 
+  class UrlParameters {
+    static getInstance() {
+      if (!UrlParameters.instance) {
+        UrlParameters.instance = new UrlParameters();
+      }
+      return UrlParameters.instance;
+    }
+
+    getURL() {
+      return new URL(window.location.href);
+    }
+
+    getBoolean(parameter, defaultValue = false) {
+      let value = this.getURL().searchParams.get(parameter);
+      return value ? value === "true" : defaultValue;
+    }
+
+    getString(parameter, defaultValue = "") {
+      return this.getURL().searchParams.get(parameter) || defaultValue;
+    }
+
+    set(parameter, newValue) {
+      const url = this.getURL();
+
+      if (newValue === false) {
+        url.searchParams.delete(parameter);
+      } else {
+        url.searchParams.set(parameter, newValue);
+      }
+
+      window.history.replaceState(null, null, url);
+      this.updateCurrentPageFooter();
+    }
+
+    setWithoutDeleting(parameter, newValue) {
+      const url = this.getURL();
+      url.searchParams.set(parameter, newValue);
+      window.history.replaceState(null, null, url);
+      this.updateCurrentPageFooter();
+    }
+
+    clear() {
+      const url = this.getURL();
+      const newPath = url.protocol + url.host;
+      const newURL = new URL(newPath);
+      window.history.replaceState(null, null, newURL);
+      this.updateCurrentPageFooter();
+    }
+
+    updateCurrentPageFooter() {
+      // console.log('window.lotivisApplication: ' + window.lotivisApplication);
+      // window.lotivisApplication.currentPage.updateFooter();
+      const url = this.getURL();
+      select("#lotivis-url-container").text(url);
+    }
+  }
+
+  UrlParameters.language = "language";
+  UrlParameters.page = "page";
+  UrlParameters.query = "query";
+  UrlParameters.searchViewMode = "search-view-mode";
+  UrlParameters.chartType = "chart-type";
+  UrlParameters.chartShowLabels = "chart-show-labels";
+  UrlParameters.chartCombineStacks = "combine-stacks";
+  UrlParameters.contentType = "content-type";
+  UrlParameters.valueType = "value-type";
+  UrlParameters.searchSensitivity = "search-sensitivity";
+  UrlParameters.startYear = "start-year";
+  UrlParameters.endYear = "end-year";
+
+  UrlParameters.showTestData = "show-samples";
+
   exports.BarChart = BarChart;
   exports.DataController = DataController;
   exports.DataItem = DataItem;
@@ -24617,6 +24689,7 @@
   exports.LabelsChart = LabelsChart;
   exports.MapChart = MapChart;
   exports.PlotChart = PlotChart;
+  exports.UrlParameters = UrlParameters;
   exports.config = LOTIVIS_CONFIG$1;
   exports.csv = csv;
   exports.csvParse = csvParse;
