9de5e475cf9ee0bdd447b5645263474527f8b295
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-02-14 12:31:51 +0100

    bugfix departments data artists generation

--- generate/generate.assets.dev.js
+++ generate/generate.assets.dev.js
@@ -4,10 +4,10 @@ console.log("[DEV] Start generating assets");
 
 // require("./datasets.artists.activity.range");
 // require("./datasets.year");
-// require("./datasets.departement.tracks");
-// require("./datasets.departement.artists");
+require("./datasets.departement.tracks");
+require("./datasets.departement.artists");
 // require("./datasets.overview");
-require("./datasets.innovation.list.js");
+// require("./datasets.innovation.list.js");
 // require("./copy.assets.js");
 // require("./about.json");
 
